a62a5504b3eb23864109c5f811cd85960a7505de
Muhammad Arslan <arslanashiq@beyonderissolutions.com>
2023-08-28 01:36:03 +0500

    image added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -253,4 +253,4 @@ export const commonDiseases = [
 console.log(commonDiseases);
 
 export const defaultImg =
-  "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80";
+  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
